9faa5b4976ff9b0cd170cc65f60cbb82a3807c4e
Alfredo Alcafuz Ramírez <alfredo@44r.cl>
2022-01-28 23:19:21 -0300

    Google Id

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcryptjs = require('bcryptjs');
 const {response} = require('express');
 const { validationResult } = require('express-validator');
 const generarJWT = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const Usuario = require('../models/usuario');
 
 const login = async (req, res = response) => {
@@ -34,7 +35,7 @@ const login = async (req, res = response) => {
     } catch (error) {
         console.log(error)
         return res.status(500).json({
-            msg: 'Cominiquese con el administrador'
+            msg: 'Comuniquese con el administrador'
         })
     }
     res.json({
@@ -42,6 +43,52 @@ const login = async (req, res = response) => {
     })
 }
 
+const googleSignIn = async (req, res = response) => {
+    const {id_token} = req.body;
+
+    try {
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: '-',
+                rol: 'USER_ROLE',
+                img,
+                google: true
+            };
+            usuario = new Usuario(data);
+            console.log({usuario});
+            await usuario.save();
+            console.log('Listo!!')
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar JWT
+        const token = await generarJWT (usuario.id);
+        
+        res.json({
+            usuario,
+            token
+        });
+        
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El Token no se pudo verificar',
+            error
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
