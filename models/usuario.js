f4ab0404fbac201c6e761ec9e3c64a1e5aa2a9c7
Alfredo Alcafuz Ramírez <alfredo@44r.cl>
2022-01-28 23:45:42 -0300

    Google Id

--- models/usuario.js
+++ models/usuario.js
@@ -19,7 +19,7 @@ const UsuarioSchema = Schema({
     rol: {
         type: String,
         required: true,
-        enum: ['ADMIN_ROLE', 'USER_ROL']
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
     },
     estado: {
         type: Boolean,
