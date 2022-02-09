1055f8d16f8a43d67acdf09c0347d1462f1ecc9b
Alfredo Alcafuz Ramírez <alfredo@44r.cl>
2022-02-09 13:39:34 -0300

    No image found

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,18 +1,25 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { cargarArchivo, actualizarImagen } = require("../controllers/uploads");
+const { cargarArchivo, actualizarImagen, mostrarImagen } = require("../controllers/uploads");
 const { coleccionesPermitidas } = require("../helpers");
 const { validarCampos, validarJWT, validarArchivoSubir } = require("../middlewares");
 
 const router = Router();
 
-router.post("/", [validarArchivoSubir], cargarArchivo);
+router.post('/', [validarArchivoSubir], cargarArchivo);
 
-router.put("/:coleccion/:id", [
+router.put('/:coleccion/:id', [
     validarArchivoSubir,
     check('id', 'El id debe ser de MongoDB').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
 ], actualizarImagen);
+
+router.get('/:coleccion/:id', [
+ //   validarArchivoSubir,
+    check('id', 'El id debe ser de MongoDB').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], mostrarImagen);
 
 module.exports = router;
