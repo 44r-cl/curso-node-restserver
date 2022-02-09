8719b6ae86db8cc354d504cc3129cd3a4f667b87
Alfredo Alcafuz Ramírez <alfredo@44r.cl>
2022-02-09 16:24:21 -0300

    Cloudinary

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require("../controllers/uploads");
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require("../controllers/uploads");
 const { coleccionesPermitidas } = require("../helpers");
 const { validarCampos, validarJWT, validarArchivoSubir } = require("../middlewares");
 
@@ -13,7 +13,8 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe ser de MongoDB').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarImagen);
+], actualizarImagenCloudinary);
+//], actualizarImagen);
 
 router.get('/:coleccion/:id', [
  //   validarArchivoSubir,
